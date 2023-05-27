7bdec2e7905940f101cea90074ab4442722aa1c9
skillhunter333 <simon.risspy@gmx.de>
2023-05-27 17:21:41 +0200

    structure changed, socketio needs to be added

--- routes/routes.js
+++ routes/routes.js
@@ -17,6 +17,18 @@ const { checkId, checkRegister, checkLogin } = require('../middlewares/validateR
 const checkCharacteristics = require('../middlewares/checkCharacteristics.js')
 const { getAllCharacteristics } = require('../controllers/characteristicControllers.js')
 const appendUsersAround = require('../middlewares/appendUsersAround')
+const {
+    accessChat,
+    fetchChats,
+    createGroupChat,
+    removeFromGroup,
+    addToGroup,
+    renameGroup,
+  } = require("../controllers/chatControllers.js");
+  const {
+    allMessages,
+    sendMessage,
+  } = require("../controllers/messageControllers");
 
 
 
@@ -30,6 +42,15 @@ Router.route('/users/me/').get(checkToken, getLoggedInUser).put(checkToken, chec
 Router.route('/users/around').get(checkToken, appendUsersAround, getUsersAround)
 Router.route('/users/getuser/:id/').get(checkToken, checkId, getUserById)
 Router.route('/users/getmatches').get(checkToken, appendUsersAround, getMatches)
+Router.route("/api/chat").post(checkToken, accessChat);
+Router.route("/api/chat").get(checkToken, fetchChats);
+Router.route("/api/chat/group").post(checkToken, createGroupChat);
+Router.route("/api/chat/rename").put(checkToken, renameGroup);
+Router.route("/api/chat/groupremove").put(checkToken, removeFromGroup);
+Router.route("/api/chat/groupadd").put(checkToken, addToGroup);
+Router.route("/api/message/:chatId").get(checkToken, allMessages);
+Router.route("/api/message").post(checkToken, sendMessage);
+
 
 
 
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const errorHandler = require('./middlewares/errorHandler.js')
 const connectDB = require('./db/connectDB.js')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const chatRoutes = require("./routes/chatRoutes")
-const messageRoutes = require("./routes/messageRoutes")
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -25,8 +23,6 @@ app.use(cookieParser())
 
 //Routes
 app.use('/', Router);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
 
 //Error handling
 app.use(errorHandler)
@@ -35,42 +31,3 @@ app.listen(port, () => {
   console.log(`server listening on port ${port}`)
 });
 
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-  socket.on("new message", (newMessageRecieved) => {
-    let chat = newMessageRecieved.chat;
-
-    if (!chat.users) return console.log("chat.users not defined");
-
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
